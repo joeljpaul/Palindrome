81a931fe5948f8554c3fdb6a98a6ed27afbd608b
Joel J Paul <67753289+joeljpaul@users.noreply.github.com>
2020-07-06 20:02:39 +0530

    Add files via upload

---
+++ Palindrome.py
@@ -0,0 +1,7 @@
+first_str = 'MALAYALAM'
+first_str = first_str.casefold()
+reverse_str = reversed(first_str)
+if list(first_str) = list(reverse_str):
+   print("This is a Palentrome")
+else:
+   print("This is not a palentrome")
