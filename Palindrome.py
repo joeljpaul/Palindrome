e1b46b10cd6da5669508fbf6f41f0fa8db46d84e
Joel J Paul <joeljpaul2004@gmail.com>
2020-07-10 20:49:36 +0530

    Updated Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -1,8 +1,11 @@
 def palindrome(str):
-   # Write code here and return True if it is a palindrome
-   return False
+    # Write code here and return True if it is a palindrome
+    reverse_str = reversed(str)
+    if list(str) == list(reverse_str):
+        return True
+    return False
 
 if palindrome("malayalam"):
-   print("It is a palindrome")
+    print("It is a palindrome")
 else:
-   print("It is not a palindrome")
+    print("It is not a palindrome")
